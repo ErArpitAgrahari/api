464d904629c2db0e7a63c91cc8cf4347dab179af
ARPIT KUMAR <arpitagrahari01@gmail.com>
2023-05-20 23:33:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TableRow from './TableRow';
 
-function App() {
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        );
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className='table1'>
+      <h1>Cryptocurrency Table</h1>
+      <table>
+        <thead>
+          <tr>
+          <th>Image</th>
+            <th>Name</th>
+            <th>ID</th>
+            
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <TableRow key={item.id} data={item} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
